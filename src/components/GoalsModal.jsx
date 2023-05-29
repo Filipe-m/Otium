779f576d8979afdab5ddb34413d0323f8f5f06eb
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2023-05-29 13:54:21 -0300

    Goals modal

--- src/components/GoalsModal.jsx
+++ src/components/GoalsModal.jsx
@@ -1,16 +1,18 @@
-import { View, Text } from 'react-native'
+import { Text, View } from 'react-native'
+import DonutChart from './DonutChart'
 
 export default function GoalsModal({ tittle, dayCount, goal }) {
+  const percentage = Math.round((dayCount * 100) / goal)
   return (
-    <View className="flex rounded-xl mt-4  w-[80%]  flex-row p-4 items-center justify-center  bg-slate-500 ">
-      <View className="p-3">
-        <View className="w-12 bg-blue-700 h-12"></View>
-      </View>
+    <View className="flex rounded-xl mt-4  w-[80vw] flex-grow flex-row p-4 items-center justify-between px-6  bg-slate-500 ">
       <View>
-        <Text className="text-2xl">{tittle}</Text>
-        <Text>
+        <Text className="text-2xl  font-semibold">{tittle}</Text>
+        <Text className="text-xl italic">
           {dayCount}/{goal} days
         </Text>
+      </View>
+      <View className="p-3">
+        <DonutChart radius={45} percentage={percentage} />
       </View>
     </View>
   )
--- src/screens/Goals.jsx
+++ src/screens/Goals.jsx
@@ -1,15 +1,35 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View, TouchableOpacity, Button } from 'react-native'
-import GoalsModal from '../components/GoalsModal'
 import { Entypo } from '@expo/vector-icons'
+import { StatusBar } from 'expo-status-bar'
+import { FlatList, TouchableOpacity, View } from 'react-native'
 import colors from 'tailwindcss/colors'
+import GoalsModal from '../components/GoalsModal'
+import goals from '../data/goals.json'
+
 const plusIconColor = colors.gray[950]
 
 export default function Goals() {
   return (
-    <View className=" items-center justify-start bg-cyan-50 flex-1">
-      <GoalsModal tittle={'No self-harm'} dayCount={2} goal={30} />
-      <GoalsModal tittle={'Kill your friends'} dayCount={13} goal={15} />
+    <View className=" items-center justify-start bg-cyan-200 flex-1">
+      <View className="flex-grow, content-start items-center ">
+        <FlatList
+          className="w-full"
+          data={goals}
+          renderItem={({ item }) => (
+            <GoalsModal
+              tittle={item.tittle}
+              dayCount={item.dayCount}
+              goal={item.goal}
+            />
+          )}
+        />
+        {/*         <GoalsModal tittle={'No self-harm'} dayCount={2} goal={30} />
+        <GoalsModal
+          tittle={"Don't kill your friends"}
+          dayCount={20}
+          goal={100}
+        /> */}
+      </View>
+
       <TouchableOpacity
         activeOpacity={0.7}
         className="absolute bottom-0 right-0 m-4 bg-slate-300 p-2 rounded-full"
